26edba2dbd0b43c202bdb6ada5360705c5c073ac
jagadeesh manne <mannejkumar@gmail.com>
2016-12-19 17:19:38 +0530

    Added role and user role domains and added liquibase script to create role and user role tables. Added liquibase script to insert seed data

--- src/main/groovy/launchpad/user/Role.groovy
+++ src/main/groovy/launchpad/user/Role.groovy
@@ -1,31 +1,26 @@
 package launchpad.user
 
+import groovy.transform.EqualsAndHashCode
 import org.hibernate.validator.constraints.Email
 
+import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
-import javax.persistence.OneToMany
 import javax.validation.constraints.NotNull
 
+@EqualsAndHashCode(includes = "authority")
 @Entity
-class User {
+class Role {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     Long id
 
     @NotNull
-    String firstName
+    String name
 
     @NotNull
-    String lastName
-
-    @Email
-    String email
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
-    Set<UserRole> userRoles
+    String authority
 }
