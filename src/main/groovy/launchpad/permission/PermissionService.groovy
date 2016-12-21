bf8e74e681e701baaf0e490331545ef52a796405
Tim Michalski <tim@lssinc.com>
2016-12-20 21:27:06 -0600

    Removed unused imports.

--- src/main/groovy/launchpad/permission/PermissionService.groovy
+++ src/main/groovy/launchpad/permission/PermissionService.groovy
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
 
-import javax.validation.ConstraintViolation
-import javax.validation.ConstraintViolationException
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
 
