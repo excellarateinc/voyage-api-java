e2415189d7ba624c529f81af4c6c25f077bd000a
Tim Michalski <tim@lssinc.com>
2016-12-21 22:56:35 -0600

    Added @Query annotations to filter out isDeleted records.

--- src/main/groovy/launchpad/role/RoleRepository.groovy
+++ src/main/groovy/launchpad/role/RoleRepository.groovy
@@ -1,6 +1,5 @@
 package launchpad.role
 
-import launchpad.user.User
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
@@ -9,4 +8,6 @@ interface RoleRepository extends CrudRepository<Role, Long> {
     @Query('FROM Role r WHERE r.id = ?1 AND r.isDeleted = false')
     Role findOne(Long id)
 
+    @Query('FROM Role r WHERE r.isDeleted = false')
+    Iterable<Role> findAll()
 }
