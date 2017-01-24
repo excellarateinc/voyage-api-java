d83bfb9ccbdcc20ef7ac9f2508a3c37b7a741a36
Laxman Bandi <laxman513@gmail.com>
2017-01-24 17:39:54 +0530

    code formatting

--- src/main/groovy/launchpad/security/user/UserPhone.groovy
+++ src/main/groovy/launchpad/security/user/UserPhone.groovy
@@ -1,19 +1,15 @@
 package launchpad.security.user
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import launchpad.security.client.Client
-import launchpad.security.client.ClientRedirectUri
 
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
-import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
-import javax.persistence.OneToMany
 import javax.validation.constraints.NotNull
 
 @Entity
