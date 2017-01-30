e0f6029552a4a299e9d4e047e8c1f9e564cddc3d
jagadeesh manne <mannejkumar@gmail.com>
2017-01-30 20:11:04 +0530

    Fixed codnarc issues

--- src/main/groovy/launchpad/security/user/UserVerifyService.groovy
+++ src/main/groovy/launchpad/security/user/UserVerifyService.groovy
@@ -13,12 +13,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
 
 import javax.validation.constraints.NotNull
 
-@Transactional
 @Service
 @Validated
 class UserVerifyService {
