d02260fd550524b6a5cc67bca28b0bf817c307af
Mythili <mythili.satyanarayana@lssinc.com>
2017-02-13 16:27:29 +0530

    Implemented Security Questions for 'Forgot Password' usecase

--- src/main/groovy/launchpad/account/AccountController.groovy
+++ src/main/groovy/launchpad/account/AccountController.groovy
@@ -12,10 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import launchpad.error.ErrorResponse
+import launchpad.error.ErrorUtils
+import launchpad.error.PasswordRecoveryFailedException
 
 /**
  * todo : unit tests, api docs after finalizing the flow of verify account
  * forgot password and render proper response.
+ * reset password, logout, password policy rules yet to be implemented
  */
 
 @RestController
@@ -23,11 +27,17 @@ import org.springframework.web.bind.annotation.RestController
 class AccountController {
     private final AccountService accountService
     private final UserVerifyService userVerifyService
+    private final SecurityQuestionService securityQuestionService
+    private final UserSecurityQuestionService userSecurityQuestionService
 
     @Autowired
-    AccountController(AccountService accountService, UserVerifyService userVerifyService) {
+    AccountController(AccountService accountService, UserVerifyService userVerifyService,
+                      SecurityQuestionService securityQuestionService,
+                      UserSecurityQuestionService userSecurityQuestionService) {
         this.accountService = accountService
         this.userVerifyService = userVerifyService
+        this.securityQuestionService = securityQuestionService
+        this.userSecurityQuestionService = userSecurityQuestionService
     }
 
     @PostMapping('/register')
@@ -57,5 +67,49 @@ class AccountController {
     ResponseEntity verify(@RequestBody String code) {
         userVerifyService.verifyCurrentUser(code)
         return new ResponseEntity(HttpStatus.NO_CONTENT)
+    }
+
+    //TODO Apidocs
+    @GetMapping('/securityquestions')
+    ResponseEntity listSecurityQuestions() {
+        Iterable<SecurityQuestion> securityQuestions = securityQuestionService.listAll()
+        return new ResponseEntity(securityQuestions, HttpStatus.OK)
+    }
+
+    //TODO Apidocs
+    @PostMapping('/securityquestions')
+    ResponseEntity saveUserSecurityAnswers(@RequestBody UserSecurityAnswers userSecurityAnswersIn) {
+        userSecurityAnswersIn.securityAnswers.each { userSecurityAnswerIn ->
+            UserSecurityQuestion userSecurityQuestion = new UserSecurityQuestion()
+            User user = new User()
+            user.id = userSecurityAnswersIn.userId
+            SecurityQuestion securityQuestion = new SecurityQuestion()
+            securityQuestion.id = userSecurityAnswerIn.questionId
+            userSecurityQuestion.user = user
+            userSecurityQuestion.question = securityQuestion
+            userSecurityQuestion.answer = userSecurityAnswerIn.answer
+            userSecurityQuestionService.saveOrUpdate(userSecurityQuestion)
+        }
+        return new ResponseEntity(HttpStatus.OK)
+    }
+
+    //TODO Apidocs
+    @PostMapping('/recovery')
+    ResponseEntity passwordRecovery(@RequestBody UserSecurityAnswers userSecurityAnswersIn) {
+        try {
+            if (userSecurityQuestionService.validateUserSecurityAnswers(userSecurityAnswersIn.userId, userSecurityAnswersIn.securityAnswers)) {
+                //TODO Call OAUTH2 authentication server for temporary token
+                return new ResponseEntity(HttpStatus.OK)
+            }
+            ErrorResponse errorResponse = new ErrorResponse(
+                    error:ErrorUtils.getErrorCode(HttpStatus.BAD_REQUEST.value()),
+                    errorDescription:'One or more answers are incorrect',)
+            return new ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
+        } catch (PasswordRecoveryFailedException ex) {
+            ErrorResponse errorResponse = new ErrorResponse(
+                    error:ErrorUtils.getErrorCode(HttpStatus.BAD_REQUEST.value()),
+                    errorDescription:ex.message,)
+            return new ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
+        }
     }
 }
