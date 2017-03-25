5f90f6bb3c67b983284b9e8fe4ec6b500bd7ddba
Tim Michalski <tim@lssinc.com>
2017-03-25 17:32:02 -0500

    Updated XssServletFilter unit tests to include script pattern testing.

--- src/test/groovy/voyage/security/XssServletFilterSpec.groovy
+++ src/test/groovy/voyage/security/XssServletFilterSpec.groovy
@@ -16,7 +16,7 @@ class XssServletFilterSpec extends Specification {
         response = Mock(HttpServletResponse)
         xssRequestWrapper = new XSSRequestWrapper(request)
     }
-
+    
     def 'filter wraps the incoming request with a XSSRequestWrapper'() {
         given:
             XssServletFilter filter = new XssServletFilter()
@@ -31,61 +31,207 @@ class XssServletFilterSpec extends Specification {
             }
     }
 
-    def 'getParameter(..) returns decoded HTML to verify that the HTML input is valid'() {
+    def 'filter returns decoded HTML to verify that the HTML input is valid'() {
         when:
-            String paramValue = xssRequestWrapper.getParameter('html')
+            String param = xssRequestWrapper.getParameter('html')
+            String header = xssRequestWrapper.getHeader('html')
 
         then:
             1 * request.getParameter('html') >> '<html><body><h1>Hello > see this &amp; </h1></body></html>'
-            paramValue == '<html><body><h1>Hello > see this & </h1></body></html>'
-
+            1 * request.getHeader('html') >> '<html><body><h1>Hello > see this &amp; </h1></body></html>'
+            param == '<html><body><h1>Hello > see this & </h1></body></html>'
+            header == '<html><body><h1>Hello > see this & </h1></body></html>'
     }
 
-    def 'getParameter(..) returns decoded CSS to verify that the CSS input is valid'() {
+    def 'filter returns decoded CSS to verify that the CSS input is valid'() {
         when:
-            String paramValue = xssRequestWrapper.getParameter('css')
+            String param = xssRequestWrapper.getParameter('css')
+            String header = xssRequestWrapper.getHeader('css')
 
         then:
             1 * request.getParameter('css') >> '.test {\\26 \\" \\26}'
-            paramValue == '.test {\26 " \26}'
+            1 * request.getHeader('css') >> '.test {\\26 \\" \\26}'
+            param == '.test {\26 " \26}'
+            header == '.test {\26 " \26}'
     }
 
-    def 'getParameter(..) returns decoded URI to verify that the URI input is valid'() {
+    def 'filter returns decoded URI to verify that the URI input is valid'() {
         when:
-            String paramValue = xssRequestWrapper.getParameter('uri')
+            String param = xssRequestWrapper.getParameter('uri')
+            String header = xssRequestWrapper.getHeader('uri')
 
         then:
             1 * request.getParameter('uri') >> '/test?param=%41%42%43%31%32%33'
-            paramValue == '/test?param=ABC123'
+            1 * request.getHeader('uri') >> '/test?param=%41%42%43%31%32%33'
+            param == '/test?param=ABC123'
+            header == '/test?param=ABC123'
     }
 
-    def 'getParameter(..) returns decoded Javascript to verify that the Javascript input is valid'() {
+    def 'filter returns decoded Javascript to verify that the Javascript input is valid'() {
         when:
-            String paramValue = xssRequestWrapper.getParameter('js')
+            String param = xssRequestWrapper.getParameter('js')
+            String header = xssRequestWrapper.getHeader('js')
 
         then:
             1 * request.getParameter('js') >> 'if (\\\'1\\\' == \'1\' || \\"1\\" == "1")'
-            paramValue == 'if (\'1\' == \'1\' || "1" == "1")'
-
+            1 * request.getHeader('js') >> 'if (\\\'1\\\' == \'1\' || \\"1\\" == "1")'
+            param == 'if (\'1\' == \'1\' || "1" == "1")'
+            header == 'if (\'1\' == \'1\' || "1" == "1")'
     }
 
-    def 'getParameter(..) returns decoded VBScript to verify that the VBScript input is valid'() {
+    def 'filter returns decoded VBScript to verify that the VBScript input is valid'() {
         when:
-            String paramValue = xssRequestWrapper.getParameter('vbs')
+            String param = xssRequestWrapper.getParameter('vbs')
+            String header = xssRequestWrapper.getHeader('vbs')
 
         then:
             1 * request.getParameter('vbs') >> '\\" something here'
-            paramValue == '" something here'
-
+            1 * request.getHeader('vbs') >> '\\" something here'
+            param == '" something here'
+            header == '" something here'
     }
 
-    def 'getParameter(..) returns decoded XML to verify that the XML input is valid'() {
+    def 'filter returns decoded XML to verify that the XML input is valid'() {
         when:
-            String paramValue = xssRequestWrapper.getParameter('xml')
+            String param = xssRequestWrapper.getParameter('xml')
+            String header = xssRequestWrapper.getHeader('xml')
 
         then:
             1 * request.getParameter('xml') >> '<xml><category><name>Something &amp; Another &#x3A3; &#198;</name></category</xml>'
-            paramValue == '<xml><category><name>Something & Another Σ Æ</name></category</xml>'
+            1 * request.getHeader('xml') >> '<xml><category><name>Something &amp; Another &#x3A3; &#198;</name></category</xml>'
+            param == '<xml><category><name>Something & Another Σ Æ</name></category</xml>'
+            header == '<xml><category><name>Something & Another Σ Æ</name></category</xml>'
+    }
 
+    def 'filter does not remove normal text'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'This is normal text < with some & symbols that are acceptable.'
+            1 * request.getHeader('script') >> 'This is normal text < with some & symbols that are acceptable.'
+            param == 'This is normal text < with some & symbols that are acceptable.'
+            header == 'This is normal text < with some & symbols that are acceptable.'
+    }
+
+    def 'filter removes suspicious scripting: <script>(.*?)</script>'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> '<script>(.*?)</script>'
+            1 * request.getHeader('script') >> '<script>(.*?)</script>'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: src[\\r\\n]*=[\\r\\n]*\'(.*?)\''() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'src=\'something\''
+            1 * request.getHeader('script') >> 'src=\'something\''
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: src[\\r\\n]*=[\\r\\n]*"(.*?)"'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'src="something"'
+            1 * request.getHeader('script') >> 'src="something"'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: </script>'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> '</script>'
+            1 * request.getHeader('script') >> '</script>'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: <script(.*?)>'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> '<script(.*?)>'
+            1 * request.getHeader('script') >> '<script(.*?)>'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: eval\\((.*?)\\)'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'eval(something)'
+            1 * request.getHeader('script') >> 'eval(something)'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: expression\\((.*?)\\)'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'expression(something)'
+            1 * request.getHeader('script') >> 'expression(something)'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: javascript:'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'javascript:'
+            1 * request.getHeader('script') >> 'javascript:'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: vbscript:'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'vbscript:'
+            1 * request.getHeader('script') >> 'vbscript:'
+            param == ''
+            header == ''
+    }
+
+    def 'filter removes suspicious scripting: onload:'() {
+        when:
+            String param = xssRequestWrapper.getParameter('script')
+            String header = xssRequestWrapper.getHeader('script')
+
+        then:
+            1 * request.getParameter('script') >> 'onload="some function"'
+            1 * request.getHeader('script') >> 'onload="some function"'
+            param == '"some function"'
+            header == '"some function"'
     }
 }
