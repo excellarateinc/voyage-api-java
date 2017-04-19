6113334595e76266bfb45778d48dcab8f0eb37a9
Jagadeesh <mannejkumar@gmail.com>
2017-04-19 23:30:38 +0530

    Overwritten default RedirectMismatchException with AppOauth2Exception to avoid the client redirect urls in the error message

--- src/main/groovy/voyage/common/error/WebResponseExceptionTranslator.groovy
+++ src/main/groovy/voyage/common/error/WebResponseExceptionTranslator.groovy
@@ -7,20 +7,21 @@ import org.springframework.security.oauth2.common.exceptions.OAuth2Exception
 import org.springframework.security.oauth2.common.exceptions.RedirectMismatchException
 import org.springframework.stereotype.Component
 import voyage.security.AppOAuth2Exception
-import voyage.security.ClientRedirectMismatchException
 
 @Component
 class WebResponseExceptionTranslator implements org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator {
     private static final String CORS_ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin'
     private static final String CORS_ACCESS_WILDCARD = '*'
+    private static final String CLIENT_REDIRECT_URL_MISMATCH_MESSAGE =
+            'The client redirect URL was not valid. To resolve this issue please use a valid redirect url.'
 
     @Override
     ResponseEntity<AppOAuth2Exception> translate(Exception e) throws Exception {
         if (e instanceof RedirectMismatchException) {
             return ResponseEntity
-                    .status(e.httpErrorCode)
+                    .status(HttpStatus.BAD_REQUEST)
                     .header(CORS_ACCESS_CONTROL_ALLOW_ORIGIN, CORS_ACCESS_WILDCARD)
-                    .body(new ClientRedirectMismatchException())
+                    .body(new AppOAuth2Exception(HttpStatus.BAD_REQUEST, CLIENT_REDIRECT_URL_MISMATCH_MESSAGE))
         } else if (e instanceof OAuth2Exception) {
             OAuth2Exception oAuth2Exception = (OAuth2Exception) e
             return ResponseEntity
