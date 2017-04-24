d8ae373b984e43d4f5aee89a122419fc4b5e4d73
Sudher <sudheerg9@gmail.com>
2017-04-24 19:38:39 +0530

    Removing unnessasary files

--- src/main/groovy/voyage/profile/ProfileController.groovy
+++ src/main/groovy/voyage/profile/ProfileController.groovy
@@ -5,11 +5,10 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import voyage.security.user.ResetPassword
+
 import voyage.security.user.User
 import voyage.security.user.UserService
 import voyage.security.verify.VerifyService
